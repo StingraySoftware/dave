16f536b0dc667209b3a89a1e8ef1703da1b27c46
dash95 <dsodhi95@gmail.com>
2016-07-05 01:08:32 +0500

    Uploading file to server

--- src/main/js/electron/index.py
+++ src/main/js/electron/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request , send_from_directory
 app = Flask(__name__)
 import json
 
@@ -18,6 +18,8 @@ from werkzeug import secure_filename
 from astropy.io import fits
 
 logging.basicConfig(filename='logfile.log', level=logging.DEBUG)
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 
 @app.route('/hello')
 def hello():
@@ -26,6 +28,27 @@ filename, file_extension = os.path.splitext('/path/to/somefile.ext')
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
+
+    upload = request.files['file']
+    
+    target = os.path.join(APP_ROOT, 'uploadeddataset')
+    
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for upload in request.files.getlist("file"):
+    
+        filename = upload.filename
+        ext = os.path.splitext(filename)[1]
+        
+        if (ext == ".txt") or (ext == ".lc"):
+            logging.debug("File supported moving on...")
+        else:
+            render_template("error.html", message="Files uploaded are not supported...")
+        destination = "/".join([target, filename])
+        
+        upload.save(destination)
+
 
     logging.debug("I reached here")
     f = request.files['file']
@@ -42,7 +65,7 @@ def upload_file():
     if not text:
       return render_template("welcome.html");
 
-    text="datasets/"+text
+    text="uploadeddataset/"+text
 
     filename, file_extension = os.path.splitext(text)
 
@@ -51,7 +74,8 @@ def upload_file():
         data = np.loadtxt(light_curve)
         Time = data[0:len(data),0]
         Rate = data[0:len(data),1]
-        Error= data[0:len(data),2]
+        Error_y= data[0:len(data),2]
+        Error_x= data[0:len(data),3]
         logging.debug(file_extension)
         logging.debug("Read txt file successfully ")
 
