73004637217fa9e3e75f85bc41723adce65e7f1b
ricardovb <rvallesblanco@gmail.com>
2017-01-16 20:59:25 +0100

    Dave on OneFile Package

--- setup/build_linux-x64.bash
+++ setup/build_linux-x64.bash
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 cd src/main/js/electron
-
 npm run build
-
 cd -
 
-\cp setup/config/deply_config.js build/DAVEApp-linux-x64/resources/app/config.js
-\cp -r src/main/resources src/main/python build/DAVEApp-linux-x64/resources
-\cp -r setup/environment.yml build/DAVEApp-linux-x64/resources
+BUILD_NAME=DAVEApp-linux-x64
+BUILD_FOLDER=build/$BUILD_NAME
+
+\cp setup/config/deply_config.js $BUILD_FOLDER/resources/app/config.js
+\cp -r src/main/resources src/main/python $BUILD_FOLDER/resources
+\cp -r setup/environment.yml $BUILD_FOLDER/resources
+
+cd build
+zip -r $BUILD_NAME.zip $BUILD_NAME
+cd -
