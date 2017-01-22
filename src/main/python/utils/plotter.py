ffe6a9e096557c5d86b506bca41bf018e3379f9f
Paul Balm <paulbalm@gmail.com>
2017-01-22 13:50:41 +0100

    adding test data and refactoring plotter.py a bit

--- src/main/python/utils/plotter.py
+++ src/main/python/utils/plotter.py
@@ -1,49 +1,45 @@
 import numpy as np
+import logging
 
 def get_plotdiv_xy(dataset, axis):
-    data = []
-    for i in range(len(axis)):
-        column = dataset.tables[axis[i]["table"]].columns[axis[i]["column"]]
-        column_data = dict()
-        column_data["values"] = column.values.tolist()
-        column_data["error_values"] = column.error_values.tolist()
-        data = np.append(data, [column_data])
-
-    return data.tolist()
+    data = build_data_list(dataset, axis)
+    return data
 
 
 def get_plotdiv_xyz(dataset, axis):
-    data = []
-    for i in range(len(axis)):
-        column = dataset.tables[axis[i]["table"]].columns[axis[i]["column"]]
-        column_data = dict()
-        column_data["values"] = column.values.tolist()
-        column_data["error_values"] = column.error_values.tolist()
-        data = np.append(data, [column_data])
+    data = build_data_list(dataset, axis)
 
     color_array = np.random.uniform(-5, 5, size=len(data[0]["values"]))
     color_data = dict()
-    color_data["values"] = color_array.tolist()
+    color_data["values"] = color_array
     data = np.append(data, [color_data])
 
     ramdom_values = np.random.uniform(-8, 8, size=len(data[0]["values"]))
-    data[2]["error_values"] = ramdom_values.tolist()
+    data[2]["error_values"] = ramdom_values
 
-    return data.tolist()
+    return data
 
 
 def get_plotdiv_scatter(dataset, axis):
-    data = []
-    for i in range(len(axis)):
-        column = dataset.tables[axis[i]["table"]].columns[axis[i]["column"]]
-        column_data = dict()
-        column_data["values"] = column.values.tolist()
-        column_data["error_values"] = column.error_values.tolist()
-        data = np.append(data, [column_data])
-
+    data = build_data_list(dataset, axis)
     amplitude_array = np.random.uniform(-5, 5, size=len(data[0]["values"]))
     amplitude_data = dict()
-    amplitude_data["values"] = amplitude_array.tolist()
+    amplitude_data["values"] = amplitude_array
     data = np.append(data, [amplitude_data])
 
-    return data.tolist()
+    return data
+
+
+def build_data_list(dataset, axis):
+    data = []
+    for i in range(len(axis)):
+        table_name = axis[i]["table"]
+        if table_name in dataset.tables:
+            column = dataset.tables[table_name].columns[axis[i]["column"]]
+            column_data = dict()
+            column_data["values"] = column.values
+            column_data["error_values"] = column.error_values
+            data = np.append(data, [column_data])
+        else:
+            logging.error("Accessing unknown table: %s" % table_name)
+    return data
