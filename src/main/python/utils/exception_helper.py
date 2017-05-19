eb70ad52a181053d7e2a87999fce3dc99bc433d4
Ricardo Valles Blanco <ricardo@MacBook-Pro-de-Ricardo.local>
2017-05-19 11:37:51 +0200

    Added print traceback to exception_helper

--- src/main/python/utils/exception_helper.py
+++ src/main/python/utils/exception_helper.py
@@ -1,12 +1,27 @@
 
 import sys
+import inspect
+
+def print_full_stack():
+    print ('Traceback (most recent call last):')
+    for item in reversed(inspect.stack()[2:]):
+        print (' File "{1}", line {2}, in {3}\n'.format(*item)),
+    for line in item[4]:
+        print (' ' + line.lstrip()),
+    for item in inspect.trace():
+        print (' File "{1}", line {2}, in {3}\n'.format(*item)),
+    for line in item[4]:
+        print (' ' + line.lstrip()),
 
 def getException(method_name):
     exc_type, exc_obj, tb = sys.exc_info()
     f = tb.tb_frame
     lineno = tb.tb_lineno
     filename = f.f_code.co_filename
-    #print ('EXCEPTION {} IN ({}, LINE {}): {}'.format(method_name, filename, lineno, exc_obj))
+
+    print ('EXCEPTION {} IN ({}, LINE {}): {}'.format(method_name, filename, lineno, exc_obj))
+    print_full_stack()
+
     return 'EXCEPTION {} IN ({}, LINE {}): {}'.format(method_name, filename, lineno, exc_obj)
 
 def getWarnMsg():
